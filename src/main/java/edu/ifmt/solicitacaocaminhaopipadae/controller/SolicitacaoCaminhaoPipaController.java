6eef9f363d4dc76262088cc52978a7b55dd74cf2
Gabriel <gabrielalexsilva2@gmail.com>
2023-10-20 15:59:34 -0400

    .

--- src/main/java/edu/ifmt/solicitacaocaminhaopipadae/controller/SolicitacaoCaminhaoPipaController.java
+++ src/main/java/edu/ifmt/solicitacaocaminhaopipadae/controller/SolicitacaoCaminhaoPipaController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import ch.qos.logback.core.model.Model;
 import edu.ifmt.solicitacaocaminhaopipadae.model.solicitacao_cp.NivelUrgencia;
 import edu.ifmt.solicitacaocaminhaopipadae.model.solicitacao_cp.SolicitacaoCaminhaoPipa;
 import edu.ifmt.solicitacaocaminhaopipadae.repository.SolicitacaoCaminhaoPipaRepository;
